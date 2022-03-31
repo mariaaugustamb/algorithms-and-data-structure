4a894d1cc84c6980154da6ebd747549d2171c9e1
Maria Augusta <mamb2@cin.ufpe.br>
2022-03-30 21:30:17 -0300

    reading file

--- rpn.java
+++ rpn.java
@@ -1,40 +1,52 @@
 import java.util.Scanner;
 import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class rpn {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Stack<Double> stack = new Stack<Double>();
 
-        while (in.hasNext()) {
-            String exp = in.nextLine();
-            Double temp1 = 0.0;
-            Double temp2 = 0.0;
+        try (BufferedReader br = Files.newBufferedReader(Paths.get("myStack.stk"))) {
+            String exp;
+            while ((exp = br.readLine()) != null) {
 
-            if (exp.equals("+")) {
-                temp1 = stack.pop();
-                temp2 = stack.pop();
-                stack.push(temp1 + temp2);
-            } else if (exp.equals("-")) {
-                temp1 = stack.pop();
-                temp2 = stack.pop();
-                stack.push(temp1 - temp2);
-            } else if (exp.equals("/")) {
-                temp1 = stack.pop();
-                temp2 = stack.pop();
-                stack.push(temp1 / temp2);
-            } else if (exp.equals("*")) {
-                temp1 = stack.pop();
-                temp2 = stack.pop();
-                stack.push(temp1 * temp2);
-            } else {
-                Double d = Double.parseDouble(exp);
-                stack.push(d);
+                // String exp = in.nextLine();
+                Double temp1 = 0.0;
+                Double temp2 = 0.0;
+
+                if (exp.equals("+")) {
+                    temp1 = stack.pop();
+                    temp2 = stack.pop();
+                    stack.push(temp1 + temp2);
+                } else if (exp.equals("-")) {
+                    temp1 = stack.pop();
+                    temp2 = stack.pop();
+                    stack.push(temp1 - temp2);
+                } else if (exp.equals("/")) {
+                    temp1 = stack.pop();
+                    temp2 = stack.pop();
+                    stack.push(temp1 / temp2);
+                } else if (exp.equals("*")) {
+                    temp1 = stack.pop();
+                    temp2 = stack.pop();
+                    stack.push(temp1 * temp2);
+                } else {
+                    Double d = Double.parseDouble(exp);
+                    stack.push(d);
+                }
+
             }
 
+            System.out.println(stack.pop());
+
+        } catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
         }
 
-        System.out.println(stack.pop());
-
     }
+
 }
